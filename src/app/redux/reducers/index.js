cc566b76b91b493449e0c76bf93331bab6431baa
A40503 <itriA40503@itri.org.tw>
2017-10-05 14:47:49 +0800

    add historyData(on Redux reducer)

--- src/app/redux/reducers/index.js
+++ src/app/redux/reducers/index.js
@@ -1,6 +1,7 @@
 import { combineReducers } from 'redux';
 import Notify from '../Notify/reducerNotify';
 import Admin from '../Admin/reducerAdmin';
+import HistoryData from '../HistoryData/reducerHistoryData';
 /*
  * We combine all reducers into a single object before updated data is dispatched (sent) to store
  * Your entire applications state (store) is just whatever gets returned from all your reducers
@@ -9,6 +10,7 @@ import Admin from '../Admin/reducerAdmin';
 const allReducers = combineReducers({
   notify: Notify,
   admin: Admin,
+  historyData: HistoryData,
 });
 
 export default allReducers;
