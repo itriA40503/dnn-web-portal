9b3b33bdfcfa5360395fa79ec57ef0da9f005376
A40503 <itriA40503@itri.org.tw>
2017-11-22 16:01:58 +0800

    remove useless object

--- src/app/redux/Menu/actionMenu.js
+++ src/app/redux/Menu/actionMenu.js
@@ -1,13 +1,1 @@
-const defaultMenu = {
-  Dashboard: false,
-  Instance: false,
-  History: false,
-  Create: false,
-  Resource: false,
-  Project: false,
-  DataSet: false,
-  Models: false,
-  Network: false,
-};
-
 export const selectMenu = currentMenu => ({ type: 'open', currentMenu });
