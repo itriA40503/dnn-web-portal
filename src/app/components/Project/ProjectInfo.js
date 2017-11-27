eb5270835fba8f930dcbc4a7545fd03dad3aca29
A40503 <itriA40503@itri.org.tw>
2017-11-27 10:57:07 +0800

    add CreateJob, CardAction

--- src/app/components/Project/ProjectInfo.js
+++ src/app/components/Project/ProjectInfo.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react';
 // i18n
 import { translate } from 'react-i18next';
-import { Card, CardTitle } from 'material-ui/Card';
+
+import { Card, CardTitle, CardActions } from 'material-ui/Card';
+
 // ICON
 import ActionAssignment from 'material-ui/svg-icons/action/assignment';
+
+import CreateJob from './CreateJob';
 
 class ProjectInfo extends Component {
   render() {
     return (
       <Card>
+        <CardActions style={{
+          zIndex: 2,
+          display: 'inline-block',
+          float: 'right',
+          right: '10px',
+        }}
+        >
+          <CreateJob />
+        </CardActions>
         <CardTitle title={<b><ActionAssignment /> {this.props.project.title}</b>} />
         <div>
           {this.props.project.data}
